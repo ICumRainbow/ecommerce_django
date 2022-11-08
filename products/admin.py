76d9446bd037fff4ccb1e404587392215f289996
Ernest <onetwo20003@gmail.com>
2022-11-09 00:04:08 +0500

    made correct display of number of likes

--- products/admin.py
+++ products/admin.py
@@ -12,27 +12,25 @@ admin.site.register(Category)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "get_formatted_price", "get_current_price", "get_discount_percentage", "get_number_of_likes", "get_average_rating")
 
-    @admin.display(ordering=True)
+    @admin.display
     def get_formatted_price(self, obj):
         return "$" + str(obj.price)
 
-    @admin.display(ordering=True)
+    @admin.display
     def get_current_price(self, obj):
         return "$" + str(round(obj.price * (100 - obj.discount_rate) / 100, 2))
 
-    @admin.display(empty_value="0", ordering=True)
+    @admin.display(empty_value="0")
     def get_discount_percentage(self, obj):
         return f"{obj.discount_rate}" + "%"
 
-    @admin.display(ordering=True)
+    @admin.display
     def get_number_of_likes(self, obj):
-        # need to override get_queryset here as well
-        result = LikedProduct.objects.filter(product=obj.id).aggregate(Count("id"))
-        return result["id__count"]
+        return obj.num_likes//2 if obj.num_likes>1 else obj.num_likes
 
     @admin.display(empty_value="0")
     def get_average_rating(self, obj):
         return round(obj.avg_rating, 2) if obj.avg_rating else obj.avg_rating
 
     def get_queryset(self, request):
-        return super().get_queryset(request).annotate(avg_rating=Avg('productreview__rating'))
+        return super().get_queryset(request).annotate(avg_rating=Avg('productreview__rating'),num_likes=Count('likedproduct__product_id'))
